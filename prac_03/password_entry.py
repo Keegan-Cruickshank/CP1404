08399b7d396fa38f4aef5d0181bb4b1a5fac18d6
Keegan Cruickshank <keeegan.c@gmail.com>
2018-08-11 20:18:25 -0700

    Refactor password program to use functions

--- prac_03/password_entry.py
+++ prac_03/password_entry.py
@@ -2,10 +2,24 @@
 Keegan Cruickshank
 """
 
-min_length_of_password = 8
-password_input = input("Enter a password: >")
-while not len(password_input) >= 8:
-    print("Password needs to be {} or more characters long".format(min_length_of_password))
+
+def main():
+    password = get_password()
+    print_hidden_password(password)
+
+
+def print_hidden_password(password):
+    for index in password:
+        print("*", end="")
+
+
+def get_password():
+    min_length_of_password = 8
     password_input = input("Enter a password: >")
-for char in password_input:
-    print("*", end="")
+    while not len(password_input) >= 8:
+        print("Password needs to be {} or more characters long".format(min_length_of_password))
+        password_input = input("Enter a password: >")
+    return password_input
+
+
+main()
