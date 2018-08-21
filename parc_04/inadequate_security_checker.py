e25c917c0749c37d8e44157be7ea925db98961a6
Keegan Cruickshank <keeegan.c@gmail.com>
2018-08-21 16:44:53 +1000

    Add Terrible User Checker

---
+++ parc_04/inadequate_security_checker.py
@@ -0,0 +1,8 @@
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+user_in_list = False
+while not user_in_list:
+    user_username = input("Enter Username: ")
+    for username in usernames:
+        if user_username == username:
+            user_in_list = True
+print("Awesome, your in.")
