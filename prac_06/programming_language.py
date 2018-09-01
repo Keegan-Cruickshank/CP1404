27d651660041c9d228110ef82bc05944a5bef0fb
Keegan Cruickshank <keeegan.c@gmail.com>
2018-09-01 21:48:31 +1000

    Finalise Languages Program

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,10 +1,16 @@
 class ProgrammingLanguage:
     # Class holds Typing, Reflection, Year
-    def __init__(self, name="", typing="", reflection="", year=0):
+    def __init__(self, name="", typing="", reflection=False, year=0):
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def __str__(self):
+        return (
+            "{}, {} Typing, Reflection={}, First appeared in {}"
+            .format(self.name, self.typing, self.reflection, self.year)
+        )
 
     def is_dynamic(self) -> bool:
         if self.typing == "Dynamic":
@@ -14,8 +20,8 @@ class ProgrammingLanguage:
 
 
 def run_tests():
-    language1 = ProgrammingLanguage("Java", "Static", "Yes", 1995)
-    language2 = ProgrammingLanguage("Python", "Dynamic", "Yes", 1991)
+    language1 = ProgrammingLanguage("Java", "Static", True, 1995)
+    language2 = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     print(language1.is_dynamic())
     print(language2.is_dynamic())
 
