c6ae9a92e274d09705bf2153f53c594cf60696ea
Keegan Cruickshank <keeegan.c@gmail.com>
2018-09-02 21:51:52 +1000

    PEP8 Format Fix

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -5,7 +5,9 @@ python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
 languages = [ruby, python, visual_basic]
-dynamic_typed_languages = [language.name for language in languages if language.typing == "Dynamic"]
+dynamic_typed_languages = [
+    language.name for language in languages if language.typing == "Dynamic"
+]
 print("The dynamically typed languages are:")
 for language in dynamic_typed_languages:
     print(language)
